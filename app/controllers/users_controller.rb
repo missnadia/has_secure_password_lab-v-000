c1f2a866ec76cc0b9799897e4bba5277b07be4f3
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-20 19:36:25 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  def index
+  end
+  
   def new
   end
 
@@ -7,9 +10,6 @@ class UsersController < ApplicationController
     return redirect_to(controller: 'users', action: 'new') unless @user.save
     session[:user_id] = @user.id
     redirect_to controller: 'users', action: 'index'
-  end
-
-  def index
   end
 
   private
