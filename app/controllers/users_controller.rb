fcfa435e3063e6fb4a03e9bdf7817984fc8c202a
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-20 19:18:59 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params).save
+    return redirect_to(controller: 'users', action: 'new') unless @user.save
+    session[:user_id] = @user.id
+    redirect_to controller: 'users', action: 'show'
   end
 
   def destroy
