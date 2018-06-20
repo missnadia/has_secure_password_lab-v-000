4341c4156d3b3892f96bd180f26dad4097c60cb0
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-20 19:23:28 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
+  resources :sessions
   resources :user
 
   # The priority is based upon order of creation: first created -> highest priority.
