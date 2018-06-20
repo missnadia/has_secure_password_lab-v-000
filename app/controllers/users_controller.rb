1b1666d007d51f79b4bfd619b7089625934f9fcb
Nadia Lee <miss.nadia.lee@gmail.com>
2018-06-20 19:02:36 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(user_params)
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name, :password, :password_digest
+    )
   end
 end
